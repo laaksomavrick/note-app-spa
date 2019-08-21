d5925cf9dd65ff2b55d3483e4cc616cb287fd374
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-08-21 19:59:37 -0400

    Login form styling, 2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { RouterModule } from '@angular/router';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { AuthenticationGuard } from './app.guard';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [AppComponent, LoginPageComponent, DashboardPageComponent],
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes), NgbModule],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes), NgbModule, FormsModule],
     providers: [AuthenticationGuard],
     bootstrap: [AppComponent],
 })
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+    public usernameFormInput = '';
 
-  constructor() { }
+    public passwordFormInput = '';
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    public ngOnInit() {
+    }
+
+    public submitForm(): void {
+        console.log(this.usernameFormInput);
+        console.log(this.passwordFormInput);
+    }
 
 }
