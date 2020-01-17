3594e9ef6fee4e79b426133473622a8ed9c2676b
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2020-01-17 14:08:39 -0500

    Toolbar css changes

--- src/app/pages/dashboard/toolbar/toolbar.component.ts
+++ src/app/pages/dashboard/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, Renderer2 } from "@angular/core";
+import { Component, OnDestroy, Renderer2 } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { AppStore } from "../../../app.store";
